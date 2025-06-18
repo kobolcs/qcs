42662d0f956f2860526efb355afea6504532e3f3
kobolcs <32861010+kobolcs@users.noreply.github.com>
2025-06-18 19:36:43 +0200

    docs: add docstring for click_with_fallback

--- robot-framework-python-tests/helpers/self_heal.py
+++ robot-framework-python-tests/helpers/self_heal.py
@@ -2,7 +2,19 @@ from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
 from Browser import Browser
 
+
 def click_with_fallback(selectors):
+    """Try clicking each selector until one succeeds.
+
+    Args:
+        selectors (Iterable[str]): Locator strings to attempt in order.
+
+    Returns:
+        str: The selector that resulted in a successful click.
+
+    Raises:
+        Exception: If none of the selectors work.
+    """
     b = BuiltIn().get_library_instance("Browser")
     for sel in selectors:
         try:
